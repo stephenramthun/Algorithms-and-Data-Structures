dc9c0829c3c611a15589441405f51c5b95a69d93
stephenramthun <stephenramthun@gmail.com>
2017-12-10 17:39:29 +0100

    Add method for sorting objects implementing the Comparable interface.

--- src/main/java/com/stephenramthun/algorithms/sorting/BubbleSort.java
+++ src/main/java/com/stephenramthun/algorithms/sorting/BubbleSort.java
@@ -1,5 +1,11 @@
 package com.stephenramthun.algorithms.sorting;
 
+/**
+ * Implementation of bubble sort.
+ *
+ * @author Stephen Ramthun
+ */
+
 public class BubbleSort {
 
     /**
@@ -15,6 +21,24 @@ public class BubbleSort {
 
             for (int i = 0; i < array.length - 1; i++) {
                 if (array[i] > array[i + 1]) {
+                    sorted = false;
+                    Utility.swap(array, i, i + 1);
+                }
+            }
+        }
+    }
+
+    public static void sort(Comparable[] array) {
+        boolean sorted = false;
+
+        while (!sorted) {
+            sorted = true;
+
+            for (int i = 0; i < array.length - 1; i++) {
+                @SuppressWarnings("unchecked")
+                int comparison = array[i].compareTo(array[i + 1]);
+
+                if (comparison > 0) {
                     sorted = false;
                     Utility.swap(array, i, i + 1);
                 }
