6572fed1e4e323e1327a9a0b753b87ba7e4fbcdf
stephenramthun <stephenramthun@gmail.com>
2017-12-12 13:01:56 +0100

    Add documentation.

--- src/main/java/com/stephenramthun/algorithms/sorting/Heapsort.java
+++ src/main/java/com/stephenramthun/algorithms/sorting/Heapsort.java
@@ -2,6 +2,13 @@ package com.stephenramthun.algorithms.sorting;
 
 import java.util.Arrays;
 
+/**
+ * Implementation of a heap sort algorithm.
+ * Builds a heap by rearranging objects in an array so that they conform to the
+ * properties of a heap structure, and then sorts the array in ascending order
+ * by taking out the smallest object one at a time.
+ */
+
 public class Heapsort {
 
     /**
