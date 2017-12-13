50c8043386ad663f20cd321574ee1833d8fb6429
stephenramthun <stephenramthun@gmail.com>
2017-12-13 12:00:06 +0100

    Add methods for graph algorithms and documentation.

--- src/main/java/com/stephenramthun/datastructures/Vertex.java
+++ src/main/java/com/stephenramthun/datastructures/Vertex.java
@@ -2,25 +2,78 @@ package com.stephenramthun.datastructures;
 
 import java.util.HashSet;
 
+/**
+ * A Vertex helper class for Graph data structures.
+ *
+ * @author Stephen Ramthun
+ */
+
 public class Vertex<V extends Comparable> implements Comparable {
     V value;
     HashSet<Vertex> edges;
+    int inDegree;
 
     public Vertex(V value) {
         this.value = value;
         this.edges = new HashSet<>();
+        this.inDegree = 0;
     }
 
+    /**
+     * Adds an edge from this Vertex to another.
+     * @param v     Vertex to add as an edge.
+     */
     public void addEdge(Vertex v) {
         edges.add(v);
     }
 
+    /**
+     * Removes aan edge from this Vertex.
+     * @param v     Vertex to remove as an edge.
+     */
     public void removeEdge(Vertex v) {
         edges.remove(v);
     }
 
+    /**
+     * Checks if this Vertex has an edge connecting it to a specified Vertex.
+     * @param v     Vertex to check if there is an edge to from this one.
+     * @return      True if the edge exists.
+     */
     public boolean hasEdge(Vertex v) {
         return edges.contains(v);
+    }
+
+    /**
+     * Returns the number of edges pointing to this one.
+     * @return  The number of edges pointing to this one.
+     */
+    public int getInDegree() {
+        return inDegree;
+    }
+
+    /**
+     * Decrements the in-degree of this Vertex. Used in graph algorithms, e.g.
+     * finding a topological ordering of the vertices in a graph.
+     */
+    public void decrementInDegree() {
+        inDegree--;
+    }
+
+    /**
+     * Return the value held by this Vertex.
+     * @return  The value held by this Vertex.
+     */
+    public V getValue() {
+        return value;
+    }
+
+    /**
+     * Returns the set of edges from this Vertex.
+     * @return  The set of edges from this Vertex.
+     */
+    public HashSet<Vertex> getEdges() {
+        return edges;
     }
 
     @Override
