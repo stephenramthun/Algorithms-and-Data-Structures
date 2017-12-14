c419aa05be1720fc254569e94c5a64bcbdd1d2d9
stephenramthun <stephenramthun@gmail.com>
2017-12-14 13:29:29 +0100

    Remove unused imports.

--- src/test/java/com/stephenramthun/datastructures/PriorityQueueTest.java
+++ src/test/java/com/stephenramthun/datastructures/PriorityQueueTest.java
@@ -1,11 +1,7 @@
 package com.stephenramthun.datastructures;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
