461ff70575f1d35561ee38b3aebfe5637a9797b7
stephenramthun <stephenramthun@gmail.com>
2017-12-15 18:06:05 +0100

    Add tests.

--- src/test/java/com/stephenramthun/datastructures/ArrayListTest.java
+++ src/test/java/com/stephenramthun/datastructures/ArrayListTest.java
@@ -2,6 +2,7 @@ package com.stephenramthun.datastructures;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
@@ -29,6 +30,65 @@ class ArrayListTest {
         expected = 100;
         a.add(expected);
         assertEquals(++size, a.size());
+        assertEquals(expected, a.get(0));
+
+        expected = 101;
+        a.add(expected);
+        assertEquals(++size, a.size());
+        assertEquals(expected, a.get(1));
+
+        expected = 250;
+        a.add(expected);
+        assertEquals(++size, a.size());
+        assertEquals(expected, a.get(2));
+
+        expected = -50;
+        a.add(expected);
+        assertEquals(++size, a.size());
+        assertEquals(expected, a.get(3));
+
+        expected = Integer.MAX_VALUE;
+        a.add(expected);
+        assertEquals(++size, a.size());
+        assertEquals(expected, a.get(4));
+
+        expected = 0;
+        a.add(expected);
+        assertEquals(++size, a.size());
+        assertEquals(expected, a.get(5));
+
+        assertNotEquals(expected, a.get(0));
+        assertNotEquals(expected, a.get(1));
+        assertNotEquals(expected, a.get(2));
+        assertNotEquals(expected, a.get(3));
+        assertNotEquals(expected, a.get(4));
+    }
+
+    @Test
+    void testInsert() {
+        ArrayList<Integer> a = new ArrayList<>();
+        int expected;
+        int size = 0;
+        int n = 100;
+
+        for (int i = 0; i < n; i++) {
+            a.add(i);
+            assertEquals(++size, a.size());
+        }
+
+        for (int i = 0; i < n; i++) {
+            a.insert(n - i - 1, i);
+            assertEquals(size, a.size());
+        }
+
+        for (int i = 0; i < n; i++) {
+            assertNotEquals(i, (int)a.get(i));
+            assertEquals(n - i - 1, (int)a.get(i));
+        }
+
+        assertFalse(a.insert(0, size + 1));
+        assertFalse(a.insert(0, size + 100));
+        assertFalse(a.insert(0, -1));
     }
 
     @Test
